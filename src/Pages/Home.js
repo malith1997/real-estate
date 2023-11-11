e90616e81a2dcd49a871f898d27a408a692f7abf
malith2cristal <malith.gayashan@cristalcode.net>
2023-11-12 03:19:27 +0530

    testimonials

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from '../componets/Nav';
+import Testi from '../componets/testimonials';
 
 
 function Home() {
   return (
     <React.Fragment>
         <Nav />
+        <Testi />
   
     </React.Fragment>
     
