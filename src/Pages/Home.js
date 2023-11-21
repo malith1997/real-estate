72fd4c558e8ffb805a031d07b8a06dbf419dc6d6
malith2cristal <malith.gayashan@cristalcode.net>
2023-11-21 19:59:11 +0530

    final change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,18 +4,20 @@ import Testi from '../componets/testimonials';
 import Sale from '../componets/sale';
 import Rent1 from '../componets/rent';
 import Formm from '../componets/form';
+import Hero from '../componets/hero';
+import Footer from '../componets/footer';
 
 
 function Home() {
   return (
     <React.Fragment>
         <Nav />
+        <Hero />
         <Sale />
         <Rent1 />
         <Testi />
         <Formm />
-        
-  
+        <Footer />
     </React.Fragment>
     
   );
